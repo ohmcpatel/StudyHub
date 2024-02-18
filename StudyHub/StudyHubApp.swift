030ce4e4fe14fcb9c61e93fe4c50650179042369
Ohm Patel <ohmpatel1212@gmail.com>
2024-02-18 13:42:56 -0500

    Initial Commit

---
+++ StudyHub/StudyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyHubApp.swift
+//  StudyHub
+//
+//  Created by Ohm Patel  on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
